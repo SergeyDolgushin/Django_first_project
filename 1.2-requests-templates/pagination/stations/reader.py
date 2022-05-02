d10373b413414c2db119b650d2440124dad8f5bc
SergeyDolgushin <94533319+SergeyDolgushin@users.noreply.github.com>
2022-05-02 10:33:58 +0300

    Update reader.py

--- 1.2-requests-templates/pagination/stations/reader.py
+++ 1.2-requests-templates/pagination/stations/reader.py
@@ -2,10 +2,6 @@ import csv
 import os
 from pathlib import Path
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# BUS_STATION_CSV = os.path.join(BASE_DIR, 'data-398-2018-08-30.csv')
-
-
 def read_data(file):
     output = []
     with open(file, newline='', encoding="utf-8") as csvfile:
