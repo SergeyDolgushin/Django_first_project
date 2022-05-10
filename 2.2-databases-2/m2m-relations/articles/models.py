24b2d77cef746f7b621ecd3b84325b6d0ca44279
SergeyDolgushin <sergei.dolgushin@mail.ru>
2022-05-10 09:18:55 +0300

    HW ready

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -6,11 +6,33 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True,
+                              verbose_name='Изображение',)
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    articles = models.ManyToManyField(
+        Article, related_name='tags', through='TagsArticles')
+
+    def __str__(self):
+        return self.name
+
+
+class TagsArticles(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(
+        Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-is_main', 'tag__name']
